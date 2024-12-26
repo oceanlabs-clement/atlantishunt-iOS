661e07edabe1c3e0b86f5f3ef5c023d067ce5970
Clementgann <clement@oceantech.asia>
2024-12-26 17:45:20 +0800

    first commit

---
+++ atlantishunt/ScoreCell.swift
@@ -0,0 +1,23 @@
+//
+//  ScoreCell.swift
+//  atlantishunt
+//
+//  Created by Clement Gan on 26/12/2024.
+//
+
+import UIKit
+
+class ScoreCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ atlantishunt/ScoreHistory.swift
@@ -0,0 +1,8 @@
+//
+//  ScoreHistory.swift
+//  atlantishunt
+//
+//  Created by Clement Gan on 26/12/2024.
+//
+
+import Foundation
